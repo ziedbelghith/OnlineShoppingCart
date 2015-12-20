f0a17a43e14f393f5a94f041cba59e10cfa099f5
ziedbelghith <zied_bel@hotmail.com>
2015-12-20 23:57:37 +0100

    20/12/2015

---
+++ OnlineShoppingCart.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShoppingCart.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingCart.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
